d25304a57b4e404c4d857331d512854f8644aaa0
Neerosh <torgash.wraith@gmail.com>
2022-10-13 19:06:56 -0300

    reorganized mongoose

--- api/resume/skills.js
+++ api/resume/skills.js
@@ -1,19 +1,19 @@
 const { createSkill,getSkills } = require('../../controllers/skillController')
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI)
-//mongoose.connection.useDb('neerosh-site')
-
 module.exports = async (req, res) => {
-  switch(req.method){
-    case 'GET':
-      getSkills(req,res)
-      break;
-    case 'POST':
-      createSkill(req,res)
-      break;
-    default:
-      res.status(404).send();
-  }
-
+  mongoose.connect(process.env.MONGODB_URI).then(() =>{
+    switch(req.method){
+      case 'GET':
+        getSkills(req,res)
+        break;
+      case 'POST':
+        //createSkill(req,res)
+        break;
+      default:
+        res.status(404).send();
+    }
+  }).catch((err) => {
+    res.status(404).send(err);
+  })
 }
