f273d95e7883dfca43c06a8707c1d1c2b51001fe
Neerosh <torgash.wraith@gmail.com>
2022-11-04 20:57:31 -0300

    Test express.static

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use('/api/resume/user/experiences', resumeExperiences)
 app.use('/api/resume/user/educations',resumeEducations)
 app.use('/api/resume/user/skills', resumeSkills)
 app.use('/api/resume/user',resumeProfile)
-app.use(express.static(__dirname + '/frontend/'));
+app.use(express.static(__dirname + '/frontend'));
 
 app.get(/.*/, (req, res) => { 
   res.sendFile(__dirname+ '/frontend/index.html')
